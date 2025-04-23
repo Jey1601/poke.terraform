d19102119b56a425823ac1684cb5b7f617da0d8e
jey1601 <fernandoespinalguevara@gmail.com>
2025-04-23 15:19:12 -0600

    final

--- acr.tf
+++ acr.tf
@@ -1,5 +1,5 @@
 resource "azurerm_container_registry" "acr" {
-    name = "acr${ lower(var.project) }${ var.environment }"
+    name = "acr${ lower(var.project) }${ var.environment }jey"
     resource_group_name = azurerm_resource_group.rg.name
     location = var.location
     sku = "Basic"
